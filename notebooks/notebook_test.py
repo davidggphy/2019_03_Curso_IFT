e84f75ecbf23c4de6eebdff0e2ba8285b2e4557e
DavidGG <david.gg.phy@gmail.com>
2019-03-07 18:45:37 +0100

    Add test notebook and python script

---
+++ notebooks/notebook_test.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.0.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import numpy as np
+
+# %%
+list_test = list(range(10))
+print(list_test)
+np.mean(list_test)
